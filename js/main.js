b5fc4d76feefefc6ab9379d7da9d0e76752671d3
zavsievich <avszak@gmail.com>
2019-01-22 21:49:07 +0300

    Created block with prices and started creating popUP

---
+++ js/main.js
@@ -0,0 +1,60 @@
+'use strict'
+
+
+/* Prices */
+
+var HIDDEN_CLASS_NAME = 'hidden';
+var TARGET_CLASS_NAME = 'target';
+var BTN_CLASS_NAME = 'btn';
+
+var targetIdToShow = 1;
+
+function prices() {
+	var targets = getElements(TARGET_CLASS_NAME)
+	var btns = getElements(BTN_CLASS_NAME)
+	btns.forEach(function (btnNode) {
+		var btnNodeId = extractId(btnNode, BTN_CLASS_NAME)
+		btnNode.addEventListener('click', function () {
+			showTarget(targets, btnNodeId)
+		})
+	})
+	showTarget(targets, targetIdToShow)
+}
+
+function getElements(type) {
+	return [].slice.call(document.querySelectorAll('.' + type)).sort(function (targetNode1, targetNode2) {
+		var target1Num = extractId(targetNode1, TARGET_CLASS_NAME)
+		var target2Num = extractId(targetNode2, TARGET_CLASS_NAME)
+		return target1Num > target2Num
+	})
+}
+
+function extractId(targetNode, baseClass) {
+	var currentClassIndex = targetNode.classList.length
+	while (currentClassIndex--) {
+		var currentClass = targetNode.classList.item(currentClassIndex)
+		var maybeIdNum = parseInt(currentClass.split('-')[1])
+		if (isNaN(maybeIdNum)) {
+			continue
+		}
+		var classStrinToValidate = baseClass + '-' + maybeIdNum
+		if (classStrinToValidate === currentClass) {
+			return maybeIdNum
+		}
+	}
+}
+
+function showTarget(targets, targetId) {
+	targets.forEach(function (targetNode, targetIndex) {
+    var currentTargetNodeId = extractId(targetNode, TARGET_CLASS_NAME)
+		if (currentTargetNodeId === targetId) {
+			targetNode.classList.remove(HIDDEN_CLASS_NAME)
+		} else {
+			targetNode.classList.add(HIDDEN_CLASS_NAME)
+		}
+	})
+}
+
+prices()
+
+/* popUp */
