dc44bdd6d8e471eec5c8a7a2323cd78dc9a84af2
zavsievich <avszak@gmail.com>
2019-01-26 14:00:47 +0300

    Deleted unnecessary code from php and some bugs fixed in css

--- php/send.php
+++ php/send.php
@@ -8,25 +8,18 @@ $sitename = "GPN service";
 
 $name = $_POST["name"];
 $number = $_POST["number"];
-$model = $_POST["model"];
-$problem = $_POST["problem"];
 
 $name = htmlspecialchars($name);
 $number = htmlspecialchars($number);
-$model = htmlspecialchars($model);
-$problem = htmlspecialchars($problem);
 
 $name = urldecode($name);
 $number = urldecode($number);
-$model = urldecode($model);
-$problem = urldecode($problem);
+
 
 $name = trim($name);
 $number = trim($number);
-$model = trim($model);
-$problem = trim($problem);
 
-$message = "Имя: $name \nТелефон: $phone \nМодель: $model\nПроблема: $problem";
+$message = "Имя: $name \nТелефон: $phone";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
