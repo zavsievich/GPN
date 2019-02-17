9c3af1343868597ffc2516c2193c1649e18b9b76
zavsievich <avszak@gmail.com>
2019-02-18 01:21:59 +0300

    Project is finished

--- php/send.php
+++ php/send.php
@@ -17,19 +17,6 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close($ch);
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Спасибо</title>
-</head>
-<body>
-<h1>Спасибо! Заявка отправлена. Наш менеджер свяжется с вами.</h1>
-</body>
-</html>
 /*$arr = array(
     'Имя: ' => $name,
     'Телефон: ' => $number
